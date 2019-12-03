949a5c19b097c10b20632f7669b5687dec16fbbe
Balazs Aron <balazsaron88@gmail.com>
2019-12-03 22:18:31 +0200

    made some modifications

--- app/src/main/java/com/example/planningpokeruser/MainActivity.java
+++ app/src/main/java/com/example/planningpokeruser/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.planningpokeruser;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.planningpokeruser.group.GroupFragment;
+import com.example.planningpokeruser.group.GroupListFragment;
+import com.example.planningpokeruser.group.model.Group;
+import com.example.planningpokeruser.group.model.Question;
 import com.example.planningpokeruser.login.LoginFragment;
+import com.example.planningpokeruser.question.QuestionFragment;
 
 public class MainActivity extends AppCompatActivity implements MainView {
 
@@ -20,6 +25,30 @@ public class MainActivity extends AppCompatActivity implements MainView {
 
     @Override
     public void showGroupListing() {
-        // TODO fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, GroupListFragment.newInstance(), "")
+                .commit();
+    }
+
+    @Override
+    public void showGroup(Group group) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, GroupFragment.newInstance(group.getId()), "group")
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void showQuestion(Question question) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, QuestionFragment.newInstance(question), "question")
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        getSupportFragmentManager().popBackStack();
     }
 }
--- app/src/main/java/com/example/planningpokeruser/MainView.java
+++ app/src/main/java/com/example/planningpokeruser/MainView.java
@@ -1,5 +1,12 @@
 package com.example.planningpokeruser;
 
+import com.example.planningpokeruser.group.model.Group;
+import com.example.planningpokeruser.group.model.Question;
+
 public interface MainView {
     void showGroupListing();
+
+    void showGroup(Group group);
+
+    void showQuestion(Question question);
 }
